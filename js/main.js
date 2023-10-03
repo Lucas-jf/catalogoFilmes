8062cb4c554650e91baf8108f75c879dc4902777
Isadora <106935739+guatxinim@users.noreply.github.com>
2023-10-02 21:50:50 -0300

    Criei criaSection( )

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const cadastraFilme = () => {
     const sintese = document.getElementsByName("sintese")[0];
     const nota = document.getElementsByName("nota")[0];
     const imagem = document.getElementsByName("imagem")[0];
-    
+
     const filme = {
         nome: nome.value,
         diretor: diretor.value,       
@@ -59,6 +59,38 @@ const cadastraFilme = () => {
             filme.imagem,
         )
     )
+}
+
+const criaSection = (nome, diretor, faixaE, sintese, nota, imagem,) => {
+    const section = document.createElement("section");
+    const aImg = document.createElement("a");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const pDiretor = document.createElement("p");
+    const pFaixa = document.createElement("p");
+    const pSintese = document.createElement("p");
+    const pNota = document.createElement("p");
+    
+    aImg.setAttribute("href", "/html/informacoes.html");
+    img.setAttribute("src", imagem);
+    img.setAttribute("alt", `Capa do filme ${nome}`);
+    section.setAttribute("id", nome);
+
+    aImg.appendChild(img);
+    section.appendChild(aImg);
+    section.appendChild(h2);
+    section.appendChild(pDiretor);
+    section.appendChild(pFaixa);
+    section.appendChild(pSintese);
+    section.appendChild(pNota);
+
+    h2.innerText = "Nome: " + nome;
+    pDiretor.innerText = "Diretor:" + diretor;
+    pFaixa.innerText = "Faixa etária:" + faixaE;
+    pSintese.innerText = "Síntese:" + sintese;
+    pNota.innerText = "Nota:" + nota;
+
+    return section;
 }
 
 window.addEventListener("load", () => {
